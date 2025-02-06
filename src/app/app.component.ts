e11a6cb16ff6a9fbb393f2cdaedf9a3e6c60a7a3
miripower <polmivi@gmail.com>
2025-02-06 18:20:05 +0100

    Primer commit Angular Copia de Pixar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FeaturedMoviesComponent } from './components/featured-movies/featured-movies.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FeaturedMoviesComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/featured-movies/featured-movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-movies',
+  imports: [],
+  templateUrl: './featured-movies.component.html',
+  styleUrl: './featured-movies.component.css'
+})
+export class FeaturedMoviesComponent {
+
+}
